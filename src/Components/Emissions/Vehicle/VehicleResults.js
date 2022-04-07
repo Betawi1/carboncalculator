0eceda3fdbd7b5a5fbafe47264f5b46ab1e0a569
Betawi1 <Rashdan.ibrahim@gmail.com>
2022-04-07 21:27:52 +0800

    Add files via upload

---
+++ src/Components/Emissions/Vehicle/VehicleResults.js
@@ -0,0 +1,36 @@
+import "../EmissionPage.css";
+
+function VehicleResults({ vehicleData, onSaveVehicleData, setVehicleResults }) {
+  return (
+    <div className="emission-popup">
+      <div>
+        <h2>Carbon Estimate:</h2>
+
+        <h4>Date:</h4>
+        <p>{vehicleData.date.slice(0, 10)}</p>
+
+        <h4>Distance:</h4>
+        <p>
+          {vehicleData.distance_value} {vehicleData.distance_unit}
+        </p>
+
+        <h4>Vehicle:</h4>
+        <p>
+          {vehicleData.vehicle_year} {vehicleData.vehicle_make}{" "}
+          {vehicleData.vehicle_model}
+        </p>
+
+        <h4>Carbon Estimate (kg): </h4>
+        <p>{vehicleData.carbon_kg}</p>
+        <div>
+          
+          <button className="button-goback" onClick={() => setVehicleResults({ ...vehicleData, id: "" })}>
+            Go Back
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default VehicleResults;
