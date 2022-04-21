ec72d8b131b82057f3faf0f38eef080e006ce725
Betawi1 <Rashdan.ibrahim@gmail.com>
2022-04-21 14:24:48 +0800

    Update NavBar.js

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,7 +2,7 @@ import "./NavBar.css";
 import { NavLink } from "react-router-dom";
 import React from "react";
 import logo from "../../images/Kyoto-Web-Logo.png";
-import dexlogo from "../../images/kyotodexlogo.png";
+
 
 function NavBar({ loggedIn }) {
   //Style for navbar links
@@ -19,21 +19,20 @@ function NavBar({ loggedIn }) {
 
 
     <div className="navbar=brand">
-      <img src={logo} height="50" alt="" class="d-inline-block align-middle mr-2"/>
+     <a href="https://www.carbondex.exchange" target="_blank">      
+       <img src={logo} height="50" alt="" class="d-inline-block align-middle mr-2"/>
+     </a>
    </div>
     
      
      
- 
-     
       <div className="user-info-container">
    
-        <h1 id="name-of-app" style={{ color: "white" }}>
+        <h1 id="name-of-app" style={{ color: "black" }}>
          CARBON FOOTPRINT CALCULATOR
         </h1>
 
-       
-
+      
       </div>
 
 
@@ -57,17 +56,7 @@ function NavBar({ loggedIn }) {
         </ul>
       </div>
 
-      <div className="navbar=brand">
-      <h5><small>Offset your carbon footprint at</small></h5> 
-      <a href="https://www.kyotocoin.io" target="_blank">
-      <img src={dexlogo} height="50" alt="" class="d-inline-block align-middle mr-2"/>
-      </a>
-   </div>
-
      
-    
-     
-
 
     </div>
   );
